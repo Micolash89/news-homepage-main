a35c9e18634c1207cb7a6e179602b5d35953228f
Micolash89 <espindolajavier2013@gmail.com>
2026-02-10 09:38:34 -0300

    updated:se  mejoro la el header agregando distintos componentes , add: libreria uishadcn para el hacer el menu mobile en el futuro.

---
+++ components/HeaderDesktop.tsx
@@ -0,0 +1,33 @@
+import { headerItems } from "../app/lib/definitions/const";
+import { ItemsLinks } from "./Header";
+
+ import React from 'react'
+ 
+ export default function HeaderDesktop() {
+   return (
+     <section className="hidden sm:flex justify-between text-base  ">
+        <a href="" className="">
+          w
+          {/* <Image
+          src="/assets/images/logo.svg"
+          alt="logo W."
+          fill
+          className="object-cover "
+          
+          /> */}
+        </a>
+
+        <nav>
+          <ul className="flex gap-10">
+            {headerItems.map((e, index) => (
+              <li key={index + "elements-items-header"}>
+                <ItemsLinks item={e} />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </section>
+
+   )
+ }
+ 

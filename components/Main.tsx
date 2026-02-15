103e50491c1d5df2c1684f5f5c144013e64404d5
Micolash89 <espindolajavier2013@gmail.com>
2026-02-15 10:33:12 -0300

    `Added AboutComponent to Main component`

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,24 +1,29 @@
 import { newItems } from "@/lib/definitions/const";
 import NewComponent from "./NewComponent";
-
+import AboutComponent from "./AboutComponent";
 
 export default function Main() {
   return (
     <>
       <main className="flex flex-col gap-10">
+        <AboutComponent />
+
         <section className="p-5 bg-very-dark-blue ">
           <h3 className="capitalize text-3xl font-bold text-primary-orange mb-7">
             New
           </h3>
           <div className="flex flex-col gap-7">
-
-          {newItems &&
-            newItems.map((e, index) => (
-              <div key={index+"new-item"+e.title}>
-                <NewComponent {...e} cant={newItems.length} index={index+1} />
-              </div>
-            ))}
-            </div>
+            {newItems &&
+              newItems.map((e, index) => (
+                <div key={index + "new-item" + e.title}>
+                  <NewComponent
+                    {...e}
+                    cant={newItems.length}
+                    index={index + 1}
+                  />
+                </div>
+              ))}
+          </div>
         </section>
 
         <section>

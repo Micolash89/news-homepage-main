5a801018edce044796ad938952dac3284970c07f
Micolash89 <espindolajavier2013@gmail.com>
2026-02-16 20:42:16 -0300

    `Refactor layout and styling in components, updatred: css modo desktop de todos los componentes`

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -1,42 +1,44 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+
 
 export default function AboutComponent() {
   return (
-      <section className="flex flex-col gap-5">
-          <div>
-            <Image
-              src={"/assets/images/image-web-3-mobile.jpg"}
-              height={20}
-              width={20}
-              alt="logo"
-              sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
-              className="bg-cover w-full h-full md:hidden"
-            />
-            <Image
-              src={"/assets/images/image-web-3-desktop.jpg"}
-              height={20}
-              width={20}
-              alt="logo"
-              sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
-              className="bg-cover w-full h-full hidden md:block"
-            />
-          </div>
+    <section className="flex flex-col gap-5 md:col-span-2 md:row-span-1 ">
+      <div className="">
+        <Image
+          src={"/assets/images/image-web-3-mobile.jpg"}
+          height={20}
+          width={20}
+          alt="logo"
+          sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
+          className="bg-cover w-full h-full md:hidden"
+        />
+        <Image
+          src={"/assets/images/image-web-3-desktop.jpg"}
+          height={20}
+          width={20}
+          alt="logo"
+          sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
+          className="bg-cover w-full h-full hidden md:block"
+        />
+      </div>
 
-          <div className="flex flex-col gap-3">
-            <h2 className="font-extrabold text-5xl">
-              The Bright Future of Web 3.0?
-            </h2>
-            <div className="flex flex-col gap-7">
-              <p className="text-dark-grayish-blue">
-                We dive into the next evolution of the web that claims to put
-                the power of the platforms back into the hands of the people.
-                But is it really fulfilling its promise?
-              </p>
-              <button className="hover:cursor-pointer uppercase text-md bg-primary-red max-w-50 py-4 font-extrabold tracking-[3]">
-                Read more
-              </button>
-            </div>
-          </div>
-        </section>
-  )
+      <div className="flex flex-col gap-3 md:flex-row md:py-5">
+        <h2 className="font-extrabold text-5xl md:text-7xl md:min-w-1/2 self-center">
+          The Bright Future of Web 3.0?
+        </h2>
+        <div className="flex flex-col gap-7 md:gap-0 md:justify-between">
+          <p className="text-dark-grayish-blue font-medium md:text-lg leading-8">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <Link href={"#"} className="hover:cursor-pointer hover:bg-very-dark-blue hover:text-white uppercase text-md text-center bg-primary-red max-w-50 py-4 font-extrabold tracking-[3] ">
+            <span>Read more</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/Main.tsx
+++ components/Main.tsx
@@ -7,14 +7,14 @@ import TopComponent from "./TopComponent";
 export default function Main() {
   return (
     <>
-      <main className="flex flex-col gap-10">
+      <main className="flex flex-col gap-10 md:grid grid-cols-3 grid-rows-1 ">
         <AboutComponent />
 
-        <section className="p-5 bg-very-dark-blue ">
-          <h3 className="capitalize text-3xl font-bold text-primary-orange mb-7">
+        <section className="p-5 md:p-7 bg-very-dark-blue md:row-span-1 md:h-[620]">
+          <h3 className="capitalize text-3xl md:text-5xl font-bold text-primary-orange mb-7 ">
             New
           </h3>
-          <div className="flex flex-col gap-7">
+          <div className="flex flex-col gap-7 md:gap-0 md:justify-between md:h-[450]">
             {newItems &&
               newItems.map((e, index) => (
                 <div key={index + "new-item" + e.title}>
@@ -28,7 +28,7 @@ export default function Main() {
           </div>
         </section>
 
-        <section className="flex flex-col gap-7">
+        <section className="flex flex-col gap-7 md:col-span-3 md:flex-row">
           {topItems &&
             topItems.length > 0 &&
             topItems.map((e, index) => (
